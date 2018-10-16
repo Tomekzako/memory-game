43904967bb6a293eba65b32a0b28647485701025
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-10-16 15:18:48 +0200

    cards listener

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const cards = Array.from(document.querySelectorAll('.card'));
 let name = "";
 let turnCounter = 0;
 
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        if (name == '') {
+            name = card.dataset.name;
+            card.style.background = (`url(img/${name}.png)`);
+            card.classList.add('active');
+        } else if (name != '' && name == card.dataset.name) {
+            card.style.background = (`url(img/${name}.png)`);
+            correctMatch(name);
+            turnCounter++;
+            name = '';
+        } else if (name != '' && name != card.dataset.name) {
+            wrongMatch(name);
+            turnCounter++;
+            name = '';
+        }
+
+        document.querySelector('.score').innerHTML = "Turn counter: " + turnCounter;
+    });
+})
+
+
 
 function correctMatch(name) {
     const cards = document.querySelectorAll(`[data-name='${name}']`);
