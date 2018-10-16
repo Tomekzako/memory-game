da80680483838230d3721f14478ee222b8af1e74
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-10-16 15:18:16 +0200

    correctMatch function

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const cards = Array.from(document.querySelectorAll('.card'));
 let name = "";
 let turnCounter = 0;
 
+
+function correctMatch(name) {
+    const cards = document.querySelectorAll(`[data-name='${name}']`);
+    cards.forEach(card => {
+        card.classList.add('correct');
+        setTimeout(() => {
+            card.style.visibility = 'hidden'
+        }, 1500);
+    });
+}
+
 function wrongMatch(name) {
     const cards = document.querySelectorAll(`[data-name='${name}']`);
     cards.forEach(card => card.style.background = ('url(img/karta.png)'));
