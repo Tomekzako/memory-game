69d622651b2370ae442ef64f231c4dc51a2408e3
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-10-17 11:04:22 +0200

    lock listener when daelaying

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const cards = Array.from(document.querySelectorAll(".card"));
 let name = "";
 let turnCounter = 0;
+let lock = false;
 
 cards.forEach(card => {
   card.addEventListener("click", () => {
     let compStyles = window.getComputedStyle(card);
     let opacity = compStyles.getPropertyValue("opacity");
-    if (opacity != 0) {
+    if (opacity != 0 && lock == false) {
+      lock = true;
       if (name == "") {
         name = card.dataset.name;
         card.style.background = `url(img/${name}.png)`;
         card.classList.add("active");
+        lock = false;
       } else if (name != "" && name == card.dataset.name) {
         card.style.background = `url(img/${name}.png)`;
         correctMatch(name);
@@ -36,6 +39,7 @@ function correctMatch(name) {
     card.classList.add("correct");
     setTimeout(() => {
       card.style.opacity = "0";
+      lock = false;
     }, 1000);
   });
 }
@@ -46,6 +50,7 @@ function wrongMatch(name) {
     setTimeout(() => {
       card.classList.remove("active");
       card.style.background = "url(img/karta.png)";
+      lock = false;
     }, 1000);
   });
 }
