0d7270776a5f1ba93b2fd4d13aa459c3762c26fd
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-10-17 10:18:31 +0200

    getComputedStyle

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const cards = Array.from(document.querySelectorAll('.card'));
+const cards = Array.from(document.querySelectorAll(".card"));
 let name = "";
 let turnCounter = 0;
 
 cards.forEach(card => {
-    card.addEventListener('click', () => {
-        if (name == '') {
-            name = card.dataset.name;
-            card.style.background = (`url(img/${name}.png)`);
-            card.classList.add('active');
-        } else if (name != '' && name == card.dataset.name) {
-            card.style.background = (`url(img/${name}.png)`);
-            correctMatch(name);
-            turnCounter++;
-            name = '';
-        } else if (name != '' && name != card.dataset.name) {
-            wrongMatch(name);
-            turnCounter++;
-            name = '';
-        }
+  card.addEventListener("click", () => {
+    let compStyles = window.getComputedStyle(card);
+    let opacity = compStyles.getPropertyValue("opacity");
+    if (opacity != 0) {
+      if (name == "") {
+        name = card.dataset.name;
+        card.style.background = `url(img/${name}.png)`;
+        card.classList.add("active");
+      } else if (name != "" && name == card.dataset.name) {
+        card.style.background = `url(img/${name}.png)`;
+        correctMatch(name);
+        turnCounter++;
+        name = "";
+      } else if (name != "" && name != card.dataset.name) {
+        wrongMatch(name);
+        turnCounter++;
+        name = "";
+      }
+    }
 
-        document.querySelector('.score').innerHTML = "Turn counter: " + turnCounter;
-    });
-})
-
-
+    document.querySelector(".score").innerHTML = "Turn counter: " + turnCounter;
+  });
+});
 
 function correctMatch(name) {
-    const cards = document.querySelectorAll(`[data-name='${name}']`);
-    cards.forEach(card => {
-        card.classList.add('correct');
-        setTimeout(() => {
-            card.style.visibility = 'hidden'
-        }, 1500);
-    });
+  const cards = document.querySelectorAll(`[data-name='${name}']`);
+  cards.forEach(card => {
+    card.classList.add("correct");
+    setTimeout(() => {
+      card.style.opacity = "0";
+    }, 1000);
+  });
 }
 
 function wrongMatch(name) {
-    const cards = document.querySelectorAll(`[data-name='${name}']`);
-    cards.forEach(card => card.style.background = ('url(img/karta.png)'));
+  const cards = document.querySelectorAll(`[data-name='${name}']`);
+  cards.forEach(card => (card.style.background = "url(img/karta.png)"));
 }
